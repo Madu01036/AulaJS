72aa6db93a608439e744ff80af9cfb2df4a5ab5c
Madu01036 <mirandasolomaomariaeduarda183@gmail.com>
2026-03-05 11:17:09 -0300

    05/03

---
+++ exercicio5.js
@@ -0,0 +1,6 @@
+document.write("<h1> Aula 01 - Exercício 05 </h1>")
+
+var lado = prompt("Digite o valor do lado do quadrado: ")
+var area = parseFloat(lado)*parseFloat(lado)
+
+document.write("<p> A área do quadrado é igual a " + area)
